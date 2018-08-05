357afda5d7845b1072b8e3c5fdc239eae5aa19d1
Sharon Kynaston <desmogal@gmail.com>
2018-08-05 17:03:43 +0100

    weekend homework - no extensions yet

---
+++ pet_shop.rb
@@ -0,0 +1,67 @@
+def pet_shop_name(shop_name)
+  return shop_name[:name]
+end
+
+def total_cash(sum)
+  return sum[:admin][:total_cash]
+end
+
+def add_or_remove_cash__add(shop, value)
+  return shop[:admin][:total_cash] +=  value
+end
+
+def add_or_remove_cash(shop, value)
+  return shop[:admin][:total_cash] +=  value
+end
+
+def pets_sold(sold)
+  return sold[:admin][:pets_sold]
+end
+
+def increase_pets_sold(shop, value)
+  return shop[:admin][:pets_sold] += value
+end
+
+def stock_count(shop)
+  return shop[:pets].count
+end
+
+def pets_by_breed(shop, breed)
+  return shop[:pets].select { |pet| pet[:breed] == breed }
+end
+
+def find_pet_by_name(shop, pet_name)
+  return shop[:pets].select { |pet| pet[:name] == pet_name }[0]
+end
+
+def remove_pet_by_name(shop, pet_name)
+  shop[:pets].delete (find_pet_by_name(shop, pet_name))
+end
+
+def add_pet_to_stock(shop, new_pet)
+  shop[:pets].push << new_pet
+  return shop[:pets].count
+end
+
+def customer_cash(person)
+  return person[:cash]
+end
+
+def remove_customer_cash(person, amount)
+  return person[:cash] -=  amount
+end
+
+def customer_pet_count(person)
+  return person[:pets].count
+end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push << new_pet
+  return customer[:pets].count
+end
+
+# def test_add_pet_to_customer
+#   customer = @customers[0]
+#   add_pet_to_customer(customer, @new_pet)
+#   assert_equal(1, customer_pet_count(customer))
+# end
